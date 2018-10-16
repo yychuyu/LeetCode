afc850d311b7b866a7c50c47e08e190d225d2040
守望 <huyanbingcn@gmail.com>
2018-10-16 15:19:38 +0800

    add some comments

--- 0263_Ugly_Number/shouwang.c
+++ 0263_Ugly_Number/shouwang.c
@@ -1,12 +1,15 @@
+/*4ms，beat 100%*/
 bool isUgly(int num) {
     if(1 == num)
         return true;
-    if(0==num)
+    if(0 == num)
         return false;
-    if(0 >num)
+    if(0 > num)
         return false;
     int temp = num;
     int flag = 0;
+
+    /*设置标识，0表明可以被该数整除*/
     int flag_2 = 0;
     int flag_3 = 0;
     int flag_5 = 0;
@@ -16,7 +19,7 @@ bool isUgly(int num) {
         if(temp%2==0 && 0 == flag_2)
         {
            // printf("2 ok\n");
-            temp = temp /2;
+            temp = temp / 2;
         }   
         else
         {
@@ -26,7 +29,7 @@ bool isUgly(int num) {
         if(temp%5 == 0 && 0 == flag_5)
         {
            // printf("5 ok\n");
-            temp = temp /5;
+            temp = temp / 5;
         }
         else
         {
@@ -36,17 +39,19 @@ bool isUgly(int num) {
         if(temp %3 == 0 && 0 == flag_3)
         {
             //printf("3 ok\n");
-            temp =temp/3;
+            temp = temp/ 3;
         }
         else
         {
             flag_3 = 1;
             flag++;
         }
+        /*如果三个都不能被除，或者最后结果为1，退出循环*/
         if(3 == flag || 1 == temp)
             break;
     }
     //printf("%d\n",temp);
+    /*最后结果为1，则表明该数为丑数*/
     if(temp == 1)
     {
         //printf("true");
