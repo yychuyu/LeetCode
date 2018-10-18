a47ad34e129653f9d6f4f4d38757b8e010a517da
Torres Ng <torresng2684cte@gmail.com>
2018-10-18 19:07:15 +0800

    remove include file

--- 0204_Count_Primes/torresng.cpp
+++ 0204_Count_Primes/torresng.cpp
@@ -15,7 +15,7 @@ Input: 10
 Output: 4
 Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 ***************************************************************/
-#include <iostream>
+
 class Solution {
 public:
     int countPrimes(int n) {
