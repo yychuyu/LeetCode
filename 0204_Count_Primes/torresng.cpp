85ae698522d7f0e66bca75a744eed5748c8e9e22
Torres Ng <torresng2684cte@gmail.com>
2018-10-18 19:06:22 +0800

    Modify No.0204

--- 0204_Count_Primes/torresng.cpp
+++ 0204_Count_Primes/torresng.cpp
@@ -15,22 +15,21 @@ Input: 10
 Output: 4
 Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 ***************************************************************/
+#include <iostream>
 class Solution {
-    bool isPrime(int n) {
-        int i;
-        for(i = 2; i <= sqrt(n); i++) {
-            if(n%i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 public:
     int countPrimes(int n) {
-        size_t count = 0;
+        if(n < 3) return 0;
+        int count = 0;
+        vector<bool> vec(n, true);
+        for(int i = 2; i*i < n; i++) {
+            if(!vec[i]) continue;
+            for(int j = i*i; j < n; j+=i) {
+                vec[j] = false;
+            }
+        }
         for(int i = 2; i < n; i++) {
-            if(isPrime(i)) count++;
+            if(vec[i]) count++;
         }
         return count;
     }
