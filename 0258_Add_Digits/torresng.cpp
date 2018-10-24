d8e62cd365c4fdc058d9dee7f3053e73b83b9525
Torres Ng <torresng2684cte@gmail.com>
2018-10-24 19:46:21 +0800

    No.258

---
+++ 0258_Add_Digits/torresng.cpp
@@ -0,0 +1,46 @@
+/***************************************************************
+*   Copyright (C) 2018 All rights reserved.
+*
+*   Auth     ：Torres Ng
+*   Create Time ：2018/10/24
+*
+***************************************************************/
+/**************************************************************
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+             Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+***************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int addDigits(int num) {
+        int mod;
+        int div;
+        do {
+            mod = num / 10;
+            div = num % 10;
+            num = mod + div;
+        } while(num >= 10);
+        return num;        
+    }
+};
+
+int main(void) {
+    Solution s = Solution();
+    cout << "38: " << s.addDigits(38) << endl;
+    
+
+    return 0;
+}
