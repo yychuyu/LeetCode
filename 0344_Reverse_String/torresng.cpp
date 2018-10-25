478bd98018b6646a385917c2fe5635542d99233e
Torres Ng <torresng2684cte@gmail.com>
2018-10-25 10:01:51 +0800

    No.0344

--- 0344_Reverse_String/torresng.cpp
+++ 0344_Reverse_String/torresng.cpp
@@ -27,6 +27,7 @@ using namespace std;
 class Solution {
 public:
     string reverseString(string s) {
+        if(s.empty()) return s;
         int i = 0, j = s.size() - 1;
         while(i < j) {
             swap(s[i++], s[j--]);
