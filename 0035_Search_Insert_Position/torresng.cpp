027a3798053a4e4643e761f82471e3822490133d
Torres Ng <torresng2684cte@gmail.com>
2018-11-02 10:56:45 +0800

    No.0035

--- 0035_Search_Insert_Position/torresng.cpp
+++ 0035_Search_Insert_Position/torresng.cpp
@@ -39,17 +39,18 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int i = 0, j = nums.size(), mid;
-        while(i < j) {
-            mid = (i + j) / 2;
-            if(nums[mid] == target) return mid;
+        int l = 0, r = nums.size(), mid;
+        while(l < r) {
+            mid = (l + r) / 2;
             if(nums[mid] > target) {
-                j = mid;
+                r = mid;
+            } else if(nums[mid] < target) {
+                l = mid + 1;
             } else {
-                i = mid + 1;
+                return mid;
             }
         }
-        return j; 
+        return r; 
     }
 };
 
