683412041ec96de2927792a5778f517f60c50992
lvxiing <37661956+lxh1997zj@users.noreply.github.com>
2018-11-08 13:38:18 +0800

    Update leetcode234.py

--- leetcode234.py
+++ leetcode234.py
@@ -2,24 +2,24 @@
 # -*- coding:utf-8 -*-
 
 # class ListNode:
-#   def __init__(self,x):
-#		self.val = x
-#		self.next = None
+#     def __init__(self,x):
+#	  self.val = x
+#	  self.next = None
 
 class Solution:   # 执行用时:140ms
-	def isPalindrome(self,head):
-		"""
+    def isPalindrome(self,head):
+	"""
         :type head: ListNode
         :rtype: bool
         """
-		s = []
-		while head != None:
-			s.append(head.val)
-			head = head.next
-		for i in range(len(s)//2):
-			if s[i] != s[len(s)-i-1]:
-				return False
-		return True
+	s = []
+	while head != None:
+	    s.append(head.val)
+	    head = head.next
+	for i in range(len(s)//2):
+	    if s[i] != s[len(s)-i-1]:
+	        return False
+	return True
 '''
 # 测试
 head = ListNode(1)
