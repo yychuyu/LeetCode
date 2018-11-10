e1dd654bbaad90a8e5c7da92f6fbdf2ed04e204e
2345oo5432 <ljh321654@126.com>
2018-11-10 19:40:59 +0800

    2345oo5432 add c.c

--- c.c
+++ c.c
@@ -1,1 +1,21 @@
-This file is added by 2345oo5432 
+/**
+Ã·Ωª’ﬂ£∫ª‘ 
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    
+    int i=0,j=0;
+    int *ret=(int*)malloc(2*sizeof(int));
+    for(i=0;i<numsSize;i++){
+        //a=nums[i];
+        for(j=i+1;j<numsSize;j++){
+            if(nums[i]+nums[j]==target){
+                ret[0]=i;
+                ret[1]=j;
+                return ret;
+            }
+        }
+        
+    }
+    return NULL;
+}
