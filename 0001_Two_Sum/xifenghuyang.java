888ec4e0ff36336fa2bccd9418c901da88c9918a
yangqiangxidian@163.com <yangqiangxidian@163.com>
2018-11-11 16:58:05 +0800

    作者：西风胡杨 2018/11/11

---
+++ 0001_Two_Sum/xifenghuyang.java
@@ -0,0 +1,21 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *  
+ * @author: xifenghuyang
+ * @Date: 2018-11-11
+ * @Time: 15:03
+ */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j< nums.length; j++){
+                if((nums[i]+nums[j])==target)
+                    return new int[] {i, j};
+            }
+        }
+        throw null;
+    }
+}
