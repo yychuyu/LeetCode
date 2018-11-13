6e15ca51665d9250ec24d4e030be07eb56309f5c
Torres Ng <torresng2684cte@gmail.com>
2018-11-13 10:28:02 +0800

    No.0069 half-interval search

--- 0069_Sqrt(x)/torresng.cpp
+++ 0069_Sqrt(x)/torresng.cpp
@@ -32,19 +32,26 @@ class Solution {
 public:
     int mySqrt(int x) {
         if(x <= 1) return x;
-        long i;
-        for(i=1; i <= x/2; i++) {
-            if(i*i > x) {
-                break;
+        int i = 0, j = x/2;
+        while(i <= j) {
+            long z = i + (j - i)/2;
+            long mult = z * z;
+            if(mult == x) {
+                return z;
+            } else if(mult > x) {
+                j = z - 1;
+            } else {
+                i = z + 1;
             }
         }
-        return i - 1;
+        return j;
     }
 };
 
 int main(void) {
     Solution s = Solution();
     cout << "4: " << s.mySqrt(4) << endl;
+    cout << "6: " << s.mySqrt(6) << endl;
     cout << "8: " << s.mySqrt(8) << endl;
     cout << "2147395600: " << s.mySqrt(2147395600) << endl;
     return 0;
