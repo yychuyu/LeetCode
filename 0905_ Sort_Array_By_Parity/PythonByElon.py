ae35a0cb7c10259a8fc29bf4e50b28dead7465e0
Rallnav <zeyuchenyu@126.com>
2018-11-13 22:30:52 +0800

    Update PythonByElon.py

--- 0905_ Sort_Array_By_Parity/PythonByElon.py
+++ 0905_ Sort_Array_By_Parity/PythonByElon.py
@@ -19,4 +19,25 @@ class Solution(object):
                 left = left + 1
                 right = right - 1
         return A
-            
+
+# 2nd
+class Solution(object):
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        56ms 99.18%
+        """
+        left = 0
+        right = len(A) - 1
+        
+        while left < right:
+            while left < right and A[left] % 2 == 0:
+                left = left + 1
+            while left < right and A[right] % 2 == 1:
+                right = right - 1
+            if left < right:
+                A[left],A[right] = A[right],A[left]
+                left = left + 1
+                right = right - 1
+        return A
