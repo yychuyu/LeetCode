6516988592fd805cb92bcc6602e483761390d52e
lvxiing <37661956+lxh1997zj@users.noreply.github.com>
2018-11-15 11:13:17 +0800

    Update leetcode905.py

--- 0905_Sort_Array_By_Parity/leetcode905.py
+++ 0905_Sort_Array_By_Parity/leetcode905.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-'leetcode-905.按奇偶排序数组:给定一个非负整数数组 A,返回一个由 A 的所有偶数元素组成的数组，后面跟 A 的所有奇数元素。你可以返回满足此条件的任何数组作为答案。'
+'leetcode-905 按奇偶排序数组:给定一个非负整数数组 A,返回一个由 A 的所有偶数元素组成的数组，后面跟 A 的所有奇数元素。你可以返回满足此条件的任何数组作为答案。'
 
 class Solution:    # 92ms
 	def sortArrayByParity1(self,A):
