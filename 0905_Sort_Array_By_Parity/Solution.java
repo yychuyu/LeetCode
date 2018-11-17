b94548f3a1acf92851e2f68db2f8b3498fbdfffb
TCFinancial_xuzuhai <TCFinancial_xuzuhai@git.cloud.tencent.com>
2018-11-17 10:26:02 +0800

    按奇偶数组排序,微信号：lionfly

---
+++ 0905_Sort_Array_By_Parity/Solution.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        if(A == null || A.length == 0 || A.length == 1)
+            return A;
+        int i = 0;
+        int j = A.length - 1;
+        while(i < j){
+            if(i < j && A[i] % 2 == 0) i++;
+            if(i < j && A[j] % 2 == 1) j--;
+            swap(A, i, j);
+        }
+        return A;
+    }
+
+    private void swap(int[] A, int i, int j){
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
+}
