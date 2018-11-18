faead025bb9b71c1f1b1cc3bfe321aa28b7b2b95
Bylant <18809428954@163.com>
2018-11-18 11:00:01 +0800

    判断是否为回文数字

---
+++ 0009_Palindrome_Number/PalindromeNumber.java
@@ -0,0 +1,37 @@
+/**
+ * Created by zhoudl on 2018/10/3.
+ */
+public class PalindromeNumber {
+
+    /*
+    Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+    Example 1:
+
+    Input: 121
+    Output: true
+    Example 2:
+
+    Input: -121
+    Output: false
+    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+    Example 3:
+
+    Input: 10
+    Output: false
+    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+    Follow up:
+    Coud you solve it without converting the integer to a string?
+
+    翻译：判断是否为回文数字
+
+
+    */
+
+    public static boolean isPalindrome(int x) {
+        return false;
+    }
+    public static void main (String[] args) {
+        isPalindrome(121);
+    }
+}
