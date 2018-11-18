cbde2c4f3e0931114eb11375f780e3aa87aaff73
yrpang <pangyiren100@gmail.com>
2018-11-18 19:29:29 +0800

    yrpang's answer to 0009

---
+++ 0009_Palindrome_Number/yrpang.c
@@ -0,0 +1,37 @@
+/***************************************************************************
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+****************************************************************************/
+
+
+
+#include<stdio.h>
+#include<stdbool.h>
+
+
+bool isPalindrome(int x) {
+
+    if(x<0) return false;
+
+    else{
+        int pop,com=0;
+        int old = x;
+        while(x!=0)
+        {
+            pop = x%10;
+            com= com * 10 + pop;
+            x = x / 10;
+        }
+        if(com == old) return true;
+        else return false;
+    }
+    
+}
+
+
+
+int main()
+{
+    int input;
+    scanf("%d",&input);
+    printf("%d",isPalindrome(input));
+}
