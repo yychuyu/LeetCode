ef5621829baf3b62331b604901ea7870250f92de
MQQM <34396349+MQQM@users.noreply.github.com>
2018-11-24 21:02:35 +0800

    Update Implement_strStr()2.cpp

--- 0028_Implement_strStr/Implement_strStr()2.cpp
+++ 0028_Implement_strStr/Implement_strStr()2.cpp
@@ -9,6 +9,7 @@
   对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
   
   参考：
+  https://www.cnblogs.com/ganganloveu/p/3753981.html
   https://blog.csdn.net/v_july_v/article/details/7041827
 */
 class Solution {
