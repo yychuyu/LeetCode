166adea1c1fa08e2ed57f584af512379b892c88c
Torres Ng <torresng2684cte@gmail.com>
2018-11-29 14:49:15 +0800

    No.0617

--- 0617_Merge_Two_Binary_Trees/torresng.cpp
+++ 0617_Merge_Two_Binary_Trees/torresng.cpp
@@ -46,7 +46,6 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if(t1 == nullptr && t2 == nullptr) return nullptr;
         if(t1 == nullptr) return t2;
         if(t2 == nullptr) return t1;
         t1->val += t2->val;
