0d7147709152a1c66c43ecefe30e56cd03ebfbee
Torres Ng <torresng2684cte@gmail.com>
2018-12-03 23:17:29 +0800

    No.0557 C

--- 0557_Reverse_Words_in_a_String_III/torresng.c
+++ 0557_Reverse_Words_in_a_String_III/torresng.c
@@ -18,24 +18,37 @@ Note: In the string, each word is separated by single space and there will not b
 #include <stdio.h>
 #include <string.h>
 
-char* reverseWords(char* s) {
-    unsigned start = 0, end = 0, len = strlen(s);
+void reverseWord(char *s, int len) {
     char temp;
-    while(end < len) {
-        while(end < len && s[end] != ' ') end++;
-        for(int i = start, j = end - 1; i < j; i++, j--) {
-            temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-        }
-        start = ++end;
+    for(int i = 0; i < len/2; i++) {
+        temp = *(s - len + i);
+        *(s - len + i) = *(s - i - 1);
+        *(s - i - 1) = temp;
     }
+}
+
+void peekforspace(char *s) {
+    if(*s == '\0') return;
+
+    int sub = 0;
+    while(*s != ' ' && *s != '\0') {
+        s++;
+        sub++;
+    }
+    reverseWord(s, sub);
+    if(*s != '\0') s++;
+    peekforspace(s);
+}
+
+char* reverseWords(char *s) {
+    peekforspace(s);
+
     return s;
 }
 
 int main(void) {
-    char* str = "Let's take LeetCode contest";
-    println("Input: \"Let's take LeetCode contest\"\nOutput: %s", reverseWords(str));
+    char str[] = "Let's take LeetCode contest";
+    printf("Input: \"Let's take LeetCode contest\"\nOutput: %s\n", reverseWords(str));
     return 0;
 }
 
