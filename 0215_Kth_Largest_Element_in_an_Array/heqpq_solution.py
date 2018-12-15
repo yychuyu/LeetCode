cf251c7d224f2b47bd6efba23a7f503a9277671c
newbieyxy <846232979@qq.com>
2018-12-15 11:36:11 +0000

    python solution using min_heap

---
+++ 0215_Kth_Largest_Element_in_an_Array/heqpq_solution.py
@@ -0,0 +1,21 @@
+# solution using heapq(min_heap)
+import heapq
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        N = len(nums)
+        if (N<k or k<=0):
+            raise Exception('Invalid input!')
+            
+        nums = [-num for num in nums] # find the kth biggest from num= find the kth smallest from -num
+        heapq.heapify(nums)
+        res = float('inf')
+        for _ in range(k):
+            res = heapq.heappop(nums)
+        return -res
+        
+        
