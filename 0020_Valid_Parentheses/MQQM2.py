f461c4fce6477df1a5095b9018b94d6718e71ab6
Tianhao Zhang <tianhao.hdu@gmail.com>
2018-12-19 19:44:00 +0800

    Create MQQM2.py

---
+++ 0020_Valid_Parentheses/MQQM2.py
@@ -0,0 +1,29 @@
+/*
+  题目：
+  给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+  
+  说明：
+  有效字符串需满足：左括号必须用相同类型的右括号闭合。左括号必须以正确的顺序闭合。
+  注意空字符串可被认为是有效字符串。
+*/
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stk = list()
+        m = {'(':')','[':']','{':'}'}
+        for i in s:
+            if i == '(' or i == '[' or i == '{':
+                stk.append(i)
+            else:
+                if len(stk) == 0:
+                    return False
+                top = stk.pop()
+                if m[top] != i:
+                    return False
+        if len(stk) == 0:
+            return True
+        
+        return False   
