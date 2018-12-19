30bc781daa994a202e22548c119de8d933b752df
Tianhao Zhang <tianhao.hdu@gmail.com>
2018-12-19 19:45:48 +0800

    Update MQQM2.py

--- 0020_Valid_Parentheses/MQQM2.py
+++ 0020_Valid_Parentheses/MQQM2.py
@@ -5,6 +5,9 @@
   说明：
   有效字符串需满足：左括号必须用相同类型的右括号闭合。左括号必须以正确的顺序闭合。
   注意空字符串可被认为是有效字符串。
+  
+  参考：
+  https://blog.csdn.net/qq_17550379/article/details/80723003
 '''
 class Solution:
     def isValid(self, s):
