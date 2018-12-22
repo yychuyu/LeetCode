32c327d8f44f839a9ff83c2bbf6f778d5b950cf8
seaflower <mikeqoo123@gmail.com>
2018-12-22 13:40:30 +0800

    performance: 新增演算法,提高速度

--- 0001_Two_Sum/seaflower.go
+++ 0001_Two_Sum/seaflower.go
@@ -8,7 +8,6 @@ import "fmt"
 // 给定 nums = [2, 7, 11, 15], target = 9
 // 因为 nums[0] + nums[1] = 2 + 7 = 9 所以返回 [0, 1]
 
-
 func main() {
 	nums := []int{11, 7, 2, 5, 6}
 	target := 9
@@ -16,14 +15,26 @@ func main() {
 }
 
 func twosum(nums []int, target int) []int {
-	var ans int
 	length := len(nums)
+	//法1
+	//var ans int
+	// for i := 0; i < length; i++ {
+	// 	for j := (i + 1); j < length; j++ {
+	// 		ans = nums[i] + nums[j]
+	// 		if ans == target {
+	// 			return []int{i, j}
+	// 		}
+	// 	}
+	// }
+	//法2
+	ansMap := make(map[int]int)
 	for i := 0; i < length; i++ {
-		for j := (i + 1); j < length; j++ {
-			ans = nums[i] + nums[j]
-			if ans == target {
-				return []int{i, j}
-			}
+		ansMap[nums[i]] = i //用map(key=數字, value=索引)存下每個數字
+	}
+	for i := 0; i < length; i++ {
+		index, ok := ansMap[target-nums[i]]
+		if ok { //如果 target-nums[i] 找的到,代表存在
+			return []int{i, index}
 		}
 	}
 	return nil
