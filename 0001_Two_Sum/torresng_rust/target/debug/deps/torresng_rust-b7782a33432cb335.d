d086fdcf151dd2d70faeee34fb1c70ce496167d6
Torres Ng <torresng2684cte@gmail.com>
2019-01-04 13:49:25 +0800

    No.0001 rust move file to correct for running

---
+++ 0001_Two_Sum/torresng_rust/target/debug/deps/torresng_rust-b7782a33432cb335.d
@@ -0,0 +1,5 @@
+/Users/weng/Code/LeetCode/LeetCode/0001_Two_Sum/torresng_rust/target/debug/deps/torresng_rust-b7782a33432cb335: src/main.rs
+
+/Users/weng/Code/LeetCode/LeetCode/0001_Two_Sum/torresng_rust/target/debug/deps/torresng_rust-b7782a33432cb335.d: src/main.rs
+
+src/main.rs:
---
+++ 0001_Two_Sum/torresng_rust/target/debug/torresng_rust.d
@@ -0,0 +1,1 @@
+/Users/weng/Code/LeetCode/LeetCode/0001_Two_Sum/torresng_rust/target/debug/torresng_rust.dSYM: /Users/weng/Code/LeetCode/LeetCode/0001_Two_Sum/torresng_rust/src/main.rs
