752b4f908a58bbeb5ded64c171b2135175d2761c
seaflower <mikeqoo123@gmail.com>
2019-01-07 13:49:09 +0800

    完成No.3 LengthOfLongestSubstring

--- 0003_Longest_Substring_Without_Repeating_Characters/seaflower.go
+++ 0003_Longest_Substring_Without_Repeating_Characters/seaflower.go
@@ -12,48 +12,30 @@ func main() {
 
 //LengthOfLongestSubstring 沒有重複字元的最長字串
 func LengthOfLongestSubstring(s string) int {
-	// var max = 0   //找到的字串長度
-	// var index int //index代表 初始化的指標
-	// var j int     //j代表 後續的指標要跟index代表來比對
-	// //特別案例
-	// if len(s) < 0 {
-	// 	return 0
-	// } else if len(s) == 1 {
-	// 	return 1
-	// } else {
-	// 	for ; index < len(s); index++ {
-	// 		for j = index + 1; j < len(s); j++ {
-	// 			fmt.Println("第1個for loop index =", index)
-	// 			if s[index:index+1] == s[j:j+1] {
-	// 				fmt.Println("找到重複的字元 =>", s[index:index+1], ",index =>", index)
-	// 				fmt.Println("字串 =>", s[index:j], ",j =>", j)
-	// 				if max < len(s[index:j]) {
-	// 					max = len(s[index:j])
-	// 				}
-	// 				break
-	// 			}
-	// 		}
-	// 	}
-	// 	if max == 0 {
-	// 		fmt.Println("MAX ==", max)
-	// 		max = len(s)
-	// 	}
-	// 	return max
-	// }
+	//暴力for loop, 超時就不貼了
 
-	m := [128]int{} // 基本的字元有127個
-	for i := 0; i < 128; i++ {
+	//法2的想法: 假設xbyauxalopmnbu
+	//a重複: 不是xbyaux最長就是, uxalopmnbu最長
+	//從頭開始, 找到重複的地方就把start指標指到重複之前的"u"再開始找
+	m := [256]int{} // ASCII 0~255
+	for i := 0; i < 256; i++ {
 		m[i] = -1
 	}
+
 	length, start := 0, -1
+
 	for i := 0; i < len(s); i++ {
-		if m[s[i]] > start { // 如果該字元重複, 紀錄該字元得最大位置
+		// 如果該字元被找過, 則紀錄該字元已經紀錄的最大位置
+		if m[s[i]] > start {
 			start = m[s[i]]
 		}
-		m[s[i]] = i           // 将存在的字符记录为键, 字符在字符串中的位置记录为值
-		if i-start > length { // 当前位置 - 已经记录的最大位置, 为该次重复子串的最大长度
+		// 將存在的字元紀錄為key, 字元在字串中的位置紀錄為value
+		m[s[i]] = i //value
+		// 當前位置 - 已經紀錄的最大位置
+		if i-start > length {
 			length = i - start
 		}
 	}
+
 	return length
 }
