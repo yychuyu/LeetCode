776570815aab78d2c3a853e2c047eb8bc6546cd1
Torres Ng <torresng2684cte@gmail.com>
2019-03-02 16:11:52 +0800

    No.0175

--- problems/0175_Combine_Two_Tables/torresng.sql
+++ problems/0175_Combine_Two_Tables/torresng.sql
@@ -41,6 +41,7 @@ Table: Address
 Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
 
     FirstName, LastName, City, State
+
 ***************************************************************/
 
 SELECT FirstName, LastName, City, State
