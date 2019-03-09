dae88245117e7f0e3da0a5b67b9e226f6411fccb
luoquan <luo.quan1982@gmail.com>
2019-03-09 12:03:53 +0800

    0034

--- problems/0034_Find_First_And_Last_Position/FindFistAndLast.java
+++ problems/0034_Find_First_And_Last_Position/FindFistAndLast.java
@@ -1,5 +1,3 @@
-package PACKAGE_NAME;
-
 /**
  * @Author luoquan
  * @Email 37629327@qq.com luo.quan@aliyun.com
@@ -7,4 +5,54 @@ package PACKAGE_NAME;
  * @Date 2019/3/9 11:50
  */
 public class FindFistAndLast {
+
+    public int[] searchRange(int[] nums, int target) {
+
+        if (null == nums || 0 == nums.length)
+            return new int[]{-1, -1};
+
+        int length = nums.length;
+        int index = searchIndex(nums, 0, nums.length - 1, target);
+        int leftIndex;
+        int rightIndex;
+        int result;
+
+        if (index < 0)
+            return new int[]{-1, -1};
+
+        leftIndex = index;
+        rightIndex = index;
+
+        while (leftIndex > 0) {
+            result = searchIndex(nums, 0, leftIndex - 1, target);
+            if (result > -1)
+                leftIndex = result;
+            else
+                break;
+        }
+
+        while (rightIndex < length - 1) {
+            result = searchIndex(nums, rightIndex + 1, length, target);
+            if (result > -1)
+                rightIndex = result;
+            else
+                break;
+        }
+
+        return new int[]{leftIndex, rightIndex};
+
+    }
+
+    private static int searchIndex(int[] arr, int left, int right, int target) {
+        if (left <= right) {
+            int middle = (left + right) / 2;
+            if (target == arr[middle])
+                return middle;
+            else if (target > arr[middle])
+                return searchIndex(arr, middle + 1, right, target);
+            else
+                return searchIndex(arr, left, middle - 1, target);
+        }
+        return -1;
+    }
 }
