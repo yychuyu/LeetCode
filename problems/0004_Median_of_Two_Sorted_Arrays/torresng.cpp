e3f2f60d9299763a8df0a670ca6c73b8554b616c
Torres Ng <torresng2684cte@gmail.com>
2019-03-29 23:30:39 +0800

    No 004 24ms

--- problems/0004_Median_of_Two_Sorted_Arrays/torresng.cpp
+++ problems/0004_Median_of_Two_Sorted_Arrays/torresng.cpp
@@ -39,33 +39,34 @@ public:
     int n = nums1.size() + nums2.size();
     int k = (n + 1) / 2;
     if (n % 2 == 0) {
-      return (findKSortedArrays(nums1, nums2, k) +
-              findKSortedArrays(nums1, nums2, k + 1)) /
+      double r1 = findKSortedArrays(nums1, 0, nums2, 0, k);
+      double r2 = findKSortedArrays(nums1, 0, nums2, 0, k + 1);
+      return (findKSortedArrays(nums1, 0, nums2, 0, k) +
+              findKSortedArrays(nums1, 0, nums2, 0, k + 1)) /
              2;
     }
-    return findKSortedArrays(nums1, nums2, k);
+    return findKSortedArrays(nums1, 0, nums2, 0, k);
   }
 
-  double findKSortedArrays(vector<int> &nums1, vector<int> &nums2, int k) {
-    if (nums1.size() > nums2.size()) {
-      return findKSortedArrays(nums2, nums1, k);
-    } else if (nums1.size() == 0) {
-      return nums2[k - 1];
+  double findKSortedArrays(vector<int> &nums1, int s1, vector<int> &nums2,
+                           int s2, int k) {
+    if (nums1.size() - s1 > nums2.size() - s2) {
+      return findKSortedArrays(nums2, s2, nums1, s1, k);
+    } else if (nums1.size() == s1) {
+      return nums2[s2 + k - 1];
     } else if (k == 1) {
-      return min(nums1[0], nums2[0]);
+      return min(nums1[s1], nums2[s2]);
     }
 
     int tmp = k / 2;
-    int pa = (tmp < nums1.size()) ? tmp : nums1.size();
+    int pa = (tmp < (nums1.size() - s1)) ? tmp : (nums1.size() - s1);
     int pb = k - pa;
-    if (nums1[pa - 1] < nums2[pb - 1]) {
-      vector<int> a(nums1.begin() + pa, nums1.end());
-      return findKSortedArrays(a, nums2, k - pa);
-    } else if (nums2[pb - 1] < nums1[pa - 1]) {
-      vector<int> a(nums2.begin() + pb, nums2.end());
-      return findKSortedArrays(nums1, a, k - pb);
+    if (nums1[s1 + pa - 1] < nums2[s2 + pb - 1]) {
+      return findKSortedArrays(nums1, s1 + pa, nums2, s2, k - pa);
+    } else if (nums2[s2 + pb - 1] < nums1[s1 + pa - 1]) {
+      return findKSortedArrays(nums1, s1, nums2, s2 + pb, k - pb);
     } else {
-      return nums1[pa - 1];
+      return nums1[s1 + pa - 1];
     }
   }
 };
@@ -84,9 +85,25 @@ void test_case_2() {
   assert(s.findMedianSortedArrays(nums1, nums2) == 2.5);
 }
 
+void test_case_3() {
+  Solution s = Solution();
+  vector<int> nums1 = {100001};
+  vector<int> nums2 = {100000};
+  assert(s.findMedianSortedArrays(nums1, nums2) == 100000.5);
+}
+
+void test_case_4() {
+  Solution s = Solution();
+  vector<int> nums1 = {1, 4};
+  vector<int> nums2 = {2, 3};
+  assert(s.findMedianSortedArrays(nums1, nums2) == 2.5);
+}
+
 int main(void) {
   test_case_1();
   test_case_2();
+  test_case_3();
+  test_case_4();
 
   return 0;
 }
