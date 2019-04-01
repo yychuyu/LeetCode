47a8e854d3f2fbdc33bf66b4331b32d3395456cf
Torres Ng <torresng2684cte@gmail.com>
2019-04-01 20:56:50 +0800

    No. 0011 format

--- problems/0011_Container_With_Most_Water/torresng.cpp
+++ problems/0011_Container_With_Most_Water/torresng.cpp
@@ -18,12 +18,10 @@ Note: You may not slant the container and n is at least 2.
 The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this
 case, the max area of water (blue section) the container can contain is 49.
 
-
-
 Example:
 
-Input: [1,8,6,2,5,4,8,3,7]
-Output: 49
+  Input: [1,8,6,2,5,4,8,3,7]
+  Output: 49
 ***************************************************************/
 
 #include <iostream>
