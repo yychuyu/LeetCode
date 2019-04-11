418fbbb847e13ed29eec7d95e9e9723ce99b69be
Torres Ng <torresng2684cte@gmail.com>
2019-04-11 22:31:33 +0800

    No. 0018 12ms

--- problems/0018_4Sum/torresng.cpp
+++ problems/0018_4Sum/torresng.cpp
@@ -39,33 +39,34 @@ class Solution {
         sort(nums.begin(), nums.end());
         vector<vector<int>> ret;
 
-        unsigned long i = 0, j = nums.size() - 1;
-        while (i < j - 2) {
-            while (i < j - 2) {
-                unsigned long l = i + 1, r = j - 1;
-                int tar = nums[i] + nums[j];
+        unsigned long n = nums.size();
+        for (unsigned long i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
+                break;
+            if (nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target)
+                continue;
+            for (unsigned long j = i + 1; j < n - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                unsigned long l = j + 1, r = n - 1;
                 while (l < r) {
-                    int sum = tar + nums[l] + nums[r];
+                    int sum = nums[i] + nums[j] + nums[l] + nums[r];
                     if (sum < target) {
                         ++l;
                     } else if (sum > target) {
                         --r;
                     } else {
-                        ret.push_back({nums[i], nums[l], nums[r], nums[j]});
+                        ret.push_back({nums[i], nums[j], nums[l], nums[r]});
                         while (++l < r && nums[l] == nums[l - 1])
                             ;
                         while (l < --r && nums[r] == nums[r + 1])
                             ;
                     }
                 }
-                while (i + 2 < --j && nums[j] == nums[j + 1])
-                    ;
             }
-            j = nums.size() - 1;
-            while (++i < j - 2 && nums[i] == nums[i - 1])
-                ;
         }
-
         return ret;
     }
 };
