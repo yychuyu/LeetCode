a4428abc46438579dc6b3fa700765314caca258e
Torres Ng <torresng2684cte@gmail.com>
2019-04-11 23:03:04 +0800

    No. 0062 O(n) space

--- problems/0062_Unique_Paths/torresng.cpp
+++ problems/0062_Unique_Paths/torresng.cpp
@@ -48,22 +48,16 @@ class Solution {
     int uniquePaths(int m, int n) {
         if (m == 1 && n == 1)
             return 1;
-        int dp[m][n];
-        memset(dp, 0, sizeof(dp));
-        dp[0][0] = 0;
-        for (int i = 1; i < m; ++i) {
-            dp[i][0] = 1;
+        int dp[m];
+        for (int i = 0; i < m; ++i) {
+            dp[i] = 1;
         }
         for (int i = 1; i < n; ++i) {
-            dp[0][i] = 1;
-        }
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            for (int j = 1; j < m; ++j) {
+                dp[j] = dp[j - 1] + dp[j];
             }
         }
-
-        return dp[m - 1][n - 1];
+        return dp[m-1];
     }
 };
 
