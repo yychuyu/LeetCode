b86f870aff3025e07835265ad685382a4dd5f2f4
JasonVV <jasonwangiii@outlook.com>
2019-04-24 10:29:24 +0800

    add 0021_Merge_Two_Sorted_Lists

---
+++ problems/0021_Merge_Two_Sorted_Lists/jasonvv.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode res = new ListNode(0);
+        ListNode now = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                now.next = l1;
+                now = now.next;
+                l1 = l1.next;
+            } else {
+                now.next = l2;
+                now = now.next;
+                l2 = l2.next;
+            }
+        }
+        while(l1 != null) {
+            now.next = l1;
+            now = now.next;
+            l1 = l1.next;
+        }
+        while(l2 != null) {
+            now.next = l2;
+            now = now.next;
+            l2 = l2.next;
+        }
+        return res.next;
+    }
+}
