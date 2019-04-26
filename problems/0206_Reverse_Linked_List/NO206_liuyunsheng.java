0502c4780fb59401d261c93b9866e980b92f441d
xjtuliuyunsheng <1575504789@qq.com>
2019-04-26 16:18:46 +0800

    liuyunsheng added a file named BO206_liuyunsheng.java

---
+++ problems/0206_Reverse_Linked_List/NO206_liuyunsheng.java
@@ -0,0 +1,19 @@
+package leetcodePart5;
+
+import LeetCodePart4.ListNode;
+
+/**
+ * @author 刘云生
+ * @date 2019/4/917:31
+ */
+public class NO206_liuyunsheng {
+    public static void main(String[] args) {
+        ListNode listNode=initListNode.init();
+        System.out.println(NO206_liuyunsheng.removeElement(listNode));
+    }
+    public static ListNode removeElement(ListNode head){
+
+        return head;
+    }
+    
+}
