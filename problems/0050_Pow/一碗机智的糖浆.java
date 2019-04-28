fa556cac620d65d38f55b380c5b3ff65133ddca7
jiang <850940554@qq.com>
2019-04-28 11:22:23 +0800

    50

---
+++ problems/0050_Pow/一碗机智的糖浆.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res = 1.0;
+        for(int i = n; i != 0; i /= 2){
+            if(i % 2 != 0){
+                res *= x;
+            }
+            x *= x;
+        }
+        return  n < 0 ? 1 / res : res;
+    }
+} 
