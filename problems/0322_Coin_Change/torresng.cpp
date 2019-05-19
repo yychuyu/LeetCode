b47143d98de642e06e68cabc3478e199136f857b
Torres Ng <torresng2684cte@gmail.com>
2019-05-19 22:08:33 +0800

    No.0322 20ms

--- problems/0322_Coin_Change/torresng.cpp
+++ problems/0322_Coin_Change/torresng.cpp
@@ -37,30 +37,77 @@ class Solution {
   public:
     int coinChange(vector<int> &coins, int amount) {
         sort(coins.begin(), coins.end());
-        vector<int> memo(amount + 1, numeric_limits<int>::max());
-        memo[0] = 0;
-        return coinChangeDFS(coins, amount, memo);
+        int res = numeric_limits<int>::max();
+        helper(coins, coins.size() - 1, amount, 0, res);
+        return (res == numeric_limits<int>::max()) ? -1 : res;
     }
 
-    int coinChangeDFS(vector<int> &coins, int target, vector<int> &memo) {
-        if (target < 0) {
+    void helper(vector<int> &coins, int start, int target, int cur, int &res) {
+        if (start < 0)
+            return;
+        if (target % coins[start] == 0) {
+            res = min(res, cur + target / coins[start]);
+            return;
         }
-        if (memo[target] != numeric_limits<int>::max()) {
-            return memo[target];
-        }
-        for (vector<int>::size_type i = 0; i < coins.size(); ++i) {
-            if (coins[i] > target) {
+        for (int i = target / coins[start]; i >= 0; --i) {
+            if (cur + i > res)
                 break;
-            }
-            int tmp = coinChangeDFS(coins, target - coins[i], memo);
-            if (tmp != -1) {
-                memo[target] = min(memo[target], tmp + 1);
-            }
+            helper(coins, start - 1, target - i * coins[start], cur + i, res);
         }
-        return memo[target] = (memo[target] == numeric_limits<int>::max())
-                                  ? -1
-                                  : memo[target];
     }
+
+    /*
+     *    // Recursive
+     *    int coinChange(vector<int> &coins, int amount) {
+     *        sort(coins.begin(), coins.end());
+     *        vector<int> memo(amount + 1, numeric_limits<int>::max());
+     *        memo[0] = 0;
+     *        return coinChangeDFS(coins, amount, memo);
+     *    }
+     *
+     *    int coinChangeDFS(vector<int> &coins, int target, vector<int> &memo) {
+     *        if (target < 0) {
+     *        }
+     *        if (memo[target] != numeric_limits<int>::max()) {
+     *            return memo[target];
+     *        }
+     *        for (vector<int>::size_type i = 0; i < coins.size(); ++i) {
+     *            if (coins[i] > target) {
+     *                break;
+     *            }
+     *            int tmp = coinChangeDFS(coins, target - coins[i], memo);
+     *            if (tmp != -1) {
+     *                memo[target] = min(memo[target], tmp + 1);
+     *            }
+     *        }
+     *        return memo[target] = (memo[target] == numeric_limits<int>::max())
+     *                                  ? -1
+     *                                  : memo[target];
+     *    }
+     */
+
+    /*
+     *    // Non-Recursive
+     *
+     *    int coinChange(vector<int> &coins, int amount) {
+     *        int res = 0;
+     *        vector<int> dp(amount + 1, numeric_limits<int>::max());
+     *        dp[0] = 0;
+     *        sort(coins.begin(), coins.end());
+     *        for (int i = 1; i <= amount; ++i) {
+     *            for (vector<int>::size_type j = 0; j < coins.size(); ++j) {
+     *                if (coins[j] > i) {
+     *                    break;
+     *                }
+     *                if (dp[i - coins[j]] != numeric_limits<int>::max()) {
+     *                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+     *                }
+     *            }
+     *        }
+     *        return (dp[amount] == numeric_limits<int>::max()) ? -1 :
+     * dp[amount];
+     *    }
+     */
 };
 
 void test_case_1() {
