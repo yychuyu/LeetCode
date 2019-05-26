8d43adedc408491183ccca52d571ee87fabc8718
Torres Ng <torresng2684cte@gmail.com>
2019-05-26 20:03:25 +0800

    No.221 dp O(n) space

--- problems/0221_Maximal_Square/torresng.cpp
+++ problems/0221_Maximal_Square/torresng.cpp
@@ -29,7 +29,34 @@ using namespace std;
 
 class Solution {
   public:
+    // Dynamic Programming O(n) space
+    int maximalSquare(vector<vector<char>> &matrix) {
+        if (matrix.empty())
+            return 0;
+        int n = matrix.size(), m = matrix[0].size(), res = 0, pre = 0;
+        vector<int> dp(m + 1, 0);
+        for (int i = 0; i < m; ++i) {
+            dp[i + 1] = matrix[0][i] - '0';
+            if (dp[i + 1] == 1)
+                res = 1;
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                int t = dp[j + 1];
+                if (matrix[i][j] == '1') {
+                    dp[j + 1] = min(pre, min(dp[j], dp[j + 1])) + 1;
+                    res = max(res, dp[j + 1]);
+                } else {
+                    dp[j + 1] = 0;
+                }
+                pre = t;
+            }
+        }
+        return res * res;
+    }
+
     // Dynamic Programming
+    /*
     int maximalSquare(vector<vector<char>> &matrix) {
         if (matrix.empty())
             return 0;
@@ -51,50 +78,51 @@ class Solution {
         }
         return res * res;
     }
+    */
 
+    // brute force
     /*
-     *    // brute force
-     *    int maximalSquare(vector<vector<char>> &matrix) {
-     *        if (matrix.size() == 0)
-     *            return 0;
-     *        int res = 0;
-     *        vector<char>::size_type n = matrix.size();
-     *        vector<vector<char>>::size_type m = matrix[0].size();
-     *        for (vector<char>::size_type i = 0; i < n; ++i) {
-     *            vector<int> v(m, 0);
-     *            for (vector<char>::size_type j = i; j < n; ++j) {
-     *                for (vector<char>::size_type k = 0; k < m; ++k) {
-     *                    if (matrix[j][k] == '1') {
-     *                        ++v[k];
-     *                    }
-     *                }
-     *                int area = getSquareArea(v, j - i + 1);
-     *                if (area == 0) {
-     *                    break;
-     *                }
-     *                res = max(res, area);
-     *            }
-     *        }
-     *        return res;
-     *    }
-     *
-     *    int getSquareArea(vector<int> &v, int k) {
-     *        if (k > v.size()) {
-     *            return 0;
-     *        };
-     *        int count = 0;
-     *        for (vector<int>::value_type val : v) {
-     *            if (val != k) {
-     *                count = 0;
-     *            } else {
-     *                ++count;
-     *            }
-     *            if (count == k)
-     *                return k * k;
-     *        }
-     *        return 0;
-     *    }
-     */
+    int maximalSquare(vector<vector<char>> &matrix) {
+        if (matrix.size() == 0)
+            return 0;
+        int res = 0;
+        vector<char>::size_type n = matrix.size();
+        vector<vector<char>>::size_type m = matrix[0].size();
+        for (vector<char>::size_type i = 0; i < n; ++i) {
+            vector<int> v(m, 0);
+            for (vector<char>::size_type j = i; j < n; ++j) {
+                for (vector<char>::size_type k = 0; k < m; ++k) {
+                    if (matrix[j][k] == '1') {
+                        ++v[k];
+                    }
+                }
+                int area = getSquareArea(v, j - i + 1);
+                if (area == 0) {
+                    break;
+                }
+                res = max(res, area);
+            }
+        }
+        return res;
+    }
+
+    int getSquareArea(vector<int> &v, int k) {
+        if (k > v.size()) {
+            return 0;
+        };
+        int count = 0;
+        for (vector<int>::value_type val : v) {
+            if (val != k) {
+                count = 0;
+            } else {
+                ++count;
+            }
+            if (count == k)
+                return k * k;
+        }
+        return 0;
+    }
+    */
 };
 
 void test_case_1() {
@@ -118,9 +146,26 @@ void test_case_3() {
     assert(Solution().maximalSquare(matrix) == 4);
 }
 
+void test_case_4() {
+    vector<vector<char>> matrix{
+        {'1'},
+    };
+    assert(Solution().maximalSquare(matrix) == 1);
+}
+
+void test_case_5() {
+    vector<vector<char>> matrix{
+        {'1', '1'},
+        {'1', '1'},
+    };
+    assert(Solution().maximalSquare(matrix) == 4);
+}
+
 int main(void) {
     test_case_1();
     test_case_2();
     test_case_3();
+    test_case_4();
+    test_case_5();
     return 0;
 }
