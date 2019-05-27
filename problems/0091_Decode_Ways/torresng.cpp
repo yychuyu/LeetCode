e3493d3f2e6235330e7abef29ea39b78332496e6
Torres Ng <torresng2684cte@gmail.com>
2019-05-27 13:52:45 +0800

    No.0091 O(1) space

--- problems/0091_Decode_Ways/torresng.cpp
+++ problems/0091_Decode_Ways/torresng.cpp
@@ -32,7 +32,6 @@ Example 2:
 ***************************************************************/
 
 #include <cassert>
-#include <iostream>
 #include <string>
 #include <vector>
 
@@ -40,6 +39,27 @@ using namespace std;
 
 class Solution {
   public:
+    // Dynamic Programming O(1) space{
+    int numDecodings(string s) {
+        if (!s.empty() && s[0] == '0')
+            return 0;
+        int a = 1, b = 1;
+        for (string::size_type i = 1; i < s.size(); ++i) {
+            int pre_a = a;
+            if (s[i] == '0')
+                a = 0;
+            if (s[i - 1] == '1' || (s[i - 1] == '2' && s[i] <= '6')) {
+                a = a + b;
+                b = pre_a;
+            } else {
+                b = a;
+            }
+        }
+        return a;
+    }
+
+    // Dynamic Programming
+    /*
     int numDecodings(string s) {
         int res = 0;
         vector<int> dp(s.size(), 0);
@@ -53,6 +73,7 @@ class Solution {
         }
         return dp.back();
     }
+    */
 };
 
 void test_case_1() {
@@ -65,8 +86,32 @@ void test_case_2() {
     assert(Solution().numDecodings(s) == 3);
 }
 
+void test_case_3() {
+    string s = "0";
+    assert(Solution().numDecodings(s) == 0);
+}
+
+void test_case_4() {
+    string s = "01";
+    assert(Solution().numDecodings(s) == 0);
+}
+
+void test_case_5() {
+    string s = "1010";
+    assert(Solution().numDecodings(s) == 1);
+}
+
+void test_case_6() {
+    string s = "24726";
+    assert(Solution().numDecodings(s) == 4);
+}
+
 int main(void) {
     test_case_1();
     test_case_2();
+    test_case_3();
+    test_case_4();
+    test_case_5();
+    test_case_6();
     return 0;
 }
