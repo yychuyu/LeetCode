527a10516d9a1bb21555297e1d3a3736a250bedd
bin <bin6160@126.com>
2019-06-01 22:42:36 +0800

    update problem 120

--- problems/0120_triangle/dev_bin_28.go
+++ problems/0120_triangle/dev_bin_28.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+	// triangle := [][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}
+	// triangle := [][]int{{-1}, {2, 3}, {1, -1, -3}}
 	triangle := [][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}
 	result := minimumTotal2(triangle)
 	fmt.Println(result)
@@ -23,26 +25,24 @@ func minimumTotal(triangle [][]int) int {
 	return triangle[0][0]
 }
 
-// 思路:
-// 从第一行开始遍历到倒数第二行
-// 逐个遍历每一行的每个元素
-// 如果当前元素是第一个，则计算出下一行第一第二个元素中较小的那个值
-// 如果当前元素不是第一个，则只需要把对应下一行的两个相邻元素的右边那个与之前计算出较小的那个元素进行比较，因为左边的已经比对过了。
+// [[-1],[2,3],[1,-1,-3]]
 func minimumTotal2(triangle [][]int) int {
 	if len(triangle) == 0 {
 		return 0
 	}
-	result := triangle[0][0]
-	for i := 0; i < len(triangle)-1; i++ {
-		rowMin := 0
-		for j := 0; j < len(triangle[i]); j++ {
-			if j == 0 {
-				rowMin = min(triangle[i+1][j], triangle[i+1][j+1])
-			} else {
-				rowMin = min(rowMin, triangle[i+1][j+1])
-			}
+	var j, left, right int
+	for i := 1; i < len(triangle); i++ {
+		triangle[i][0] = triangle[i-1][0] + triangle[i][0]
+		for j = 1; j < len(triangle[i])-1; j++ {
+			right = triangle[i-1][j]
+			left = triangle[i-1][j-1]
+			triangle[i][j] += min(left, right)
 		}
-		result += rowMin
+		triangle[i][i] = triangle[i-1][i-1] + triangle[i][i]
+	}
+	result := triangle[len(triangle)-1][0]
+	for _, value := range triangle[len(triangle)-1] {
+		result = min(result, value)
 	}
 	return result
 }
