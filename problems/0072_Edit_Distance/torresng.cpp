c6d1cf7fbde9aaf0c7c66e93e0726ce6b6562552
Torres Ng <torresng2684cte@gmail.com>
2019-06-03 19:14:20 +0800

    No.0072 dp

--- problems/0072_Edit_Distance/torresng.cpp
+++ problems/0072_Edit_Distance/torresng.cpp
@@ -46,6 +46,29 @@ using namespace std;
 class Solution {
   public:
     int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        dp[0][0] = 0;
+        for (string::size_type i = 0; i <= n; ++i) {
+            for (string::size_type j = 0; j <= m; ++j) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = max(i, j);
+                } else if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] =
+                        min(dp[i - 1][j], min(dp[i - 1][j - 1], dp[i][j - 1])) +
+                        1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    // Memo
+    /*
+    int minDistance(string word1, string word2) {
         vector<vector<int>> memo(word1.size(), vector<int>(word2.size(), -1));
         return helper(word1, 0, word2, 0, memo);
     }
@@ -69,6 +92,7 @@ class Solution {
         }
         return memo[i][j] = res;
     }
+    */
 };
 
 void test_case_1() { assert(Solution().minDistance("horse", "ros") == 3); }
