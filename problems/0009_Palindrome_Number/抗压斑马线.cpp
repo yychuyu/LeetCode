8fa4d1e2ed2a2e46bfe20ede379f8c25dc4ba8a6
YobeZhou <smilezyb@163.com>
2019-06-08 13:45:16 +0800

    0009_Palindrome Number

--- problems/0009_Palindrome_Number/抗压斑马线.cpp
+++ problems/0009_Palindrome_Number/抗压斑马线.cpp
@@ -1,5 +1,5 @@
 /* 
-<<<<<<< HEAD
+<<<<<<< HEAD   
     Palindrome Number
         Determine whether an integer is a palindrome. 
         An integer is a palindrome when it reads the same backward as forward.
