8478145abb9867e6711de199ebe6f9270cfabd13
Baiyusoup <1575727840@qq.com>
2019-06-12 23:19:29 +0800

    add qingcaibaiyutang17.c

---
+++ problems/0876_Middle_of_the_Linked_List/qingcaibaiyutang17.c
@@ -0,0 +1,8 @@
+﻿struct ListNode* middleNode(struct ListNode* head){
+    struct ListNode *fast=head,*slow=head;
+    while(fast&&fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
