1656c2a3348ceda2fb88bede686c77677b2af09a
lionxu <xuzuhai163@163.com>
2019-06-13 16:27:56 +0800

    [0091和0303]

---
+++ problems/0303_Range_Sum_Query/solution.py
@@ -0,0 +1,8 @@
+# 实现比较简单，主要利用python的切片操作
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def sumRange(self, i: int, j: int) -> int:
+        return sum(self.nums[i:j+1])
