962c20e54d818f576b89a655bbb0fede4e8b02d2
OLIVERSUN <44011673+DCOLIVERSUN@users.noreply.github.com>
2019-06-14 10:31:58 +0800

    Create 4_秩秩斯干.cpp
    Answer to 0050_Pow

---
+++ problems/0050_Pow/4_秩秩斯干.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double fastPow(double x, long long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+};
