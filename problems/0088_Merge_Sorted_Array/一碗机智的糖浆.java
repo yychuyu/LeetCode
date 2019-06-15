a83ffe4092c88fa75b9407e8512c2fccfb18b381
jiang <850940554@qq.com>
2019-06-15 22:43:00 +0800

    88

---
+++ problems/0088_Merge_Sorted_Array/一碗机智的糖浆.java
@@ -0,0 +1,25 @@
+class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    // Make a copy of nums1.
+    int [] nums1_copy = new int[m];
+    System.arraycopy(nums1, 0, nums1_copy, 0, m);
+
+    // Two get pointers for nums1_copy and nums2.
+    int p1 = 0;
+    int p2 = 0;
+
+    // Set pointer for nums1
+    int p = 0;
+
+    // Compare elements from nums1_copy and nums2
+    // and add the smallest one into nums1.
+    while ((p1 < m) && (p2 < n))
+      nums1[p++] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+
+    // if there are still elements to add
+    if (p1 < m)
+      System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 - p2);
+    if (p2 < n)
+      System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+  }
+}
