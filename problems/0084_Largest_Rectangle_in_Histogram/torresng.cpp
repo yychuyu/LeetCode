2f2ee91744f6647e5b70cce4aac418705674d7ca
Torres Ng <torresng2684cte@gmail.com>
2019-06-17 00:24:32 +0800

    No.0084 12ms

--- problems/0084_Largest_Rectangle_in_Histogram/torresng.cpp
+++ problems/0084_Largest_Rectangle_in_Histogram/torresng.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <iostream>
+#include <stack>
 #include <vector>
 
 using namespace std;
@@ -7,19 +8,41 @@ using namespace std;
 class Solution {
   public:
     int largestRectangleArea(vector<int> &heights) {
+        heights.push_back(0);
+        int n = heights.size();
+        int res = 0;
+        stack<int> s;
+        s.push(-1);
+
+        for (int i = 0; i < heights.size(); ++i) {
+            int x = s.top();
+            if (x == -1 || heights[x] <= heights[i]) {
+                s.push(i);
+            } else {
+                s.pop();
+                res = max(res, heights[x] * (i - s.top() - 1));
+                --i;
+            }
+        }
+
+        return res;
+    }
+
+    // O(n ^ 2)
+    /*
+    int largestRectangleArea(vector<int> &heights) {
         int res = 0;
         for (int i = 0; i < heights.size(); ++i) {
-            // 注意這里要先判斷是否到尾了再判斷後面 || 條件，否則在Leetcode上會超出範圍報heap Error
-            if (i == heights.size() - 1 || heights[i] > heights[i + 1]) {
-                int m = heights[i];
-                for (int j = i; j >= 0; --j) {
-                    m = min(m, heights[j]);
-                    res = max(res, (i - j + 1) * m);
+            // 注意這里要先判斷是否到尾了再判斷後面 ||
+    條件，否則在Leetcode上會超出範圍報heap Error if (i == heights.size() - 1 ||
+    heights[i] > heights[i + 1]) { int m = heights[i]; for (int j = i; j >= 0;
+    --j) { m = min(m, heights[j]); res = max(res, (i - j + 1) * m);
                 }
             }
         }
         return res;
     }
+    */
 };
 
 void test_case_1() {
