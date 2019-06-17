10d838e6ffe35118bfd31bfa48a3a0d4736cb419
Torres Ng <torresng2684cte@gmail.com>
2019-06-17 10:30:51 +0800

    No.0084 comment

--- problems/0084_Largest_Rectangle_in_Histogram/torresng.cpp
+++ problems/0084_Largest_Rectangle_in_Histogram/torresng.cpp
@@ -8,11 +8,11 @@ using namespace std;
 class Solution {
   public:
     int largestRectangleArea(vector<int> &heights) {
-        heights.push_back(0);
+        heights.push_back(0);  // 處理邊界情況
         int n = heights.size();
         int res = 0;
         stack<int> s;
-        s.push(-1);
+        s.push(-1);  // 處理棧邊界情況
 
         for (int i = 0; i < heights.size(); ++i) {
             while(s.top() != -1 && heights[s.top()] > heights[i]) {
