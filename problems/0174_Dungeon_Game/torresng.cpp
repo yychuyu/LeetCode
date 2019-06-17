6281a27cfafa148bd7ce07dda005d8b155ab1ae0
Torres Ng <torresng2684cte@gmail.com>
2019-06-17 14:16:28 +0800

    No.0174

--- problems/0174_Dungeon_Game/torresng.cpp
+++ problems/0174_Dungeon_Game/torresng.cpp
@@ -44,8 +44,10 @@ Note:
 enters and the bottom-right room where the princess is imprisoned.
 ***************************************************************/
 
+#include <algorithm>
 #include <cassert>
 #include <iostream>
+#include <limits>
 #include <vector>
 
 using namespace std;
@@ -53,30 +55,17 @@ using namespace std;
 class Solution {
   public:
     int calculateMinimumHP(vector<vector<int>> &dungeon) {
-        int n = dungeon.size(), m = dungeon[0].size();
-        vector<vector<int>> dp(n, vector<int>(m));
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = m - 1; j >= 0; --j) {
-                if (i == n - 1 && j == m - 1) {
-                    dp[i][j] = dungeon[i][j] > 0 ? 1 : 1 - dungeon[i][j];
-                } else if (j == m - 1) {
-                    dp[i][j] = (dp[i + 1][j] - dungeon[i][j]) > 0
-                                   ? (dp[i + 1][j] - dungeon[i][j])
-                                   : 1;
-                } else if (i == n - 1) {
-                    dp[i][j] = (dp[i][j + 1] - dungeon[i][j]) > 0
-                                   ? (dp[i][j + 1] - dungeon[i][j])
-                                   : 1;
-                } else {
-                    int a = (dp[i + 1][j] - dungeon[i][j]) > 0
-                                ? (dp[i + 1][j] - dungeon[i][j])
-                                : 1;
+        int rows = dungeon.size(), cols = dungeon[0].size();
+        vector<vector<int>> dp(
+            rows + 1, vector<int>(cols + 1, numeric_limits<int>::max()));
 
-                    int b = (dp[i][j + 1] - dungeon[i][j]) > 0
-                                ? (dp[i][j + 1] - dungeon[i][j])
-                                : 1;
-                    dp[i][j] = min(a, b);
-                }
+        dp[rows][cols - 1] = 1;
+        dp[rows - 1][cols] = 1;
+
+        for (int i = rows - 1; i >= 0; --i) {
+            for (int j = cols - 1; j >= 0; --j) {
+                int minValue = min(dp[i][j + 1], dp[i + 1][j]) - dungeon[i][j];
+                dp[i][j] = minValue <= 0 ? 1 : minValue;
             }
         }
 
